b119aff6d5adbf7a5878db3e9bae6846b9966937
hjnicholas <182066415@qq.com>
2015-12-10 21:35:39 +0800

    add MVC proj

---
+++ XT/UL.AXT/Controllers/ShowOrderController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+
+namespace UL.AXT.Controllers
+{
+    public class ShowOrderController : Controller
+    {
+        // GET: ShowOrder
+        public ActionResult Index()
+        {
+            BLL.ShowOrder showOrder = new BLL.ShowOrder();
+            List<ShowOrderModel> lst = showOrder.GetShowingOrders().ToList();
+            return View(lst);
+        }
+    }
+}
---
+++ XT/UL.AXT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UL.AXT.Startup))]
+namespace UL.AXT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
