92532e7d6f15c8565b54eec71e9918e3f27b4441
hjnicholas <182066415@qq.com>
2015-12-13 13:08:32 +0800

    add product Model

---
+++ XT/Model/ProductModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class ProductModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDesc { get; set; }
+        public int ProductType { get; set; }
+        public int PeriodId { get; set; }
+        public string PeriodNum { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int JoinedNum { get; set; }
+
+        public DateTime CreateTime { get; set; }
+        public DateTime ProductExpires { get; set; }
+    }
+}
