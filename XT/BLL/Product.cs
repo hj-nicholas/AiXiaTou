a2eeebf9dcb7b00756b936e62d2ea6174993e131
hjnicholas <182066415@qq.com>
2015-12-13 13:10:37 +0800

    add product bll

---
+++ XT/BLL/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace BLL
+{
+    public class Product
+    {
+        public IList<ProductModel> GetProducts(int proType)
+        {
+            DAL.Product pro = new DAL.Product();
+            IList<ProductModel> lst = pro.GetProducts(proType);
+            return lst;
+        }
+    }
+}
