c5a89ddc4916472b67b875ebd435c0da975bd8c8
hjnicholas <182066415@qq.com>
2015-12-17 00:04:01 +0800

    add userInfo

---
+++ XT/BLL/UserInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace BLL
+{
+    public class UserInfo
+    {
+        DAL.UserInfo user = new DAL.UserInfo();
+
+        public UserDTO GetUserInfo(int userId)
+        {
+           UserDTO userInfo= user.GetUserInfo(userId);
+            return userInfo;
+        }
+
+        public IList<T_UserAddr> GetReceiveAddrs(int userId)
+        {
+            var addrs = user.GetReceiveAddrs(userId);
+            return addrs;
+        }
+    }
+}
