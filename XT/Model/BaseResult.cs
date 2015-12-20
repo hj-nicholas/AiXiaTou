c6c573abb69004695f1896b19b538566686b95e4
hjnicholas <182066415@qq.com>
2015-12-20 23:59:32 +0800

    repair project function

---
+++ XT/Model/BaseResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+   public class BaseResult
+    {
+        public BaseResult()
+        {
+            Errors = new List<string>();
+        }
+
+        public bool Succeeded { get; set; }
+       public string ErrMsg { get; set; }
+       public List<string> Errors { get; set; }
+        public string ResultId { get; set; }
+
+    }
+}
