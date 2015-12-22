cf58cd293a732696f7a9a383dd7efcaae5629367
hjnicholas <182066415@qq.com>
2015-12-22 23:53:13 +0800

    add wechat pay

---
+++ XT/UL.AXT/Common/WeChat/WxPayApi/Exception.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace Hoo.WeChat.WxPayAPI
+{
+    public class WxPayException : Exception 
+    {
+        public WxPayException(string msg) : base(msg) 
+        {
+
+        }
+     }
+}
